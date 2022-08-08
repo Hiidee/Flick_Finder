22b10ba65fdf31d30d8b4b384afab602435ddd50
luke natili <lukenatili123@gmail.com>
2022-08-08 14:14:37 -0400

    database structured

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,13 +1,61 @@
-BEGIN TRANSACTION;
+BEGIN TRANSACTION ;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, person, genre, movies, movies_person, movies_genre;
 
 CREATE TABLE users (
-	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
+	user_id serial,
+	username varchar(25) NOT NULL,
+	password text NOT NULL,
+	salt text NOT NULL,
+	role varchar(10) NOT NULL,
+
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-COMMIT TRANSACTION;
+CREATE TABLE person (
+	person_id serial,
+	name varchar(50) NOT NULL,
+	birthday date NOT NULL,
+
+	CONSTRAINT PK_person PRIMARY KEY (person_id)
+);
+
+CREATE TABLE genre (
+	genre_id serial,
+	name varchar(50) NOT NULL,
+
+	CONSTRAINT PK_genre PRIMARY KEY (genre_id)
+);
+
+CREATE TABLE movies (
+	movie_id serial,
+	title varchar(50),
+	overview text,
+	poster text,
+	director_id int,
+	runtime time,
+	date_released date,
+
+	CONSTRAINT PK_movies PRIMARY KEY (movie_id),
+	CONSTRAINT FK_movies_director FOREIGN KEY (director_id) REFERENCES person(person_id)
+);
+
+CREATE TABLE movies_person (
+	movie_id int NOT NULL,
+	person_id int NOT NULL,
+
+	CONSTRAINT PK_movies_person PRIMARY KEY (movie_id, person_id),
+	CONSTRAINT FK_movies_person_movies FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+	CONSTRAINT FK_movies_person_person FOREIGN KEY (person_id) REFERENCES person(person_id)
+);
+
+CREATE TABLE movies_genre (
+	movie_id int NOT NULL,
+	genre_id int NOT NULL,
+
+	CONSTRAINT PK_movies_genre PRIMARY KEY (movie_id, genre_id),
+	CONSTRAINT FK_movies_genre_movies FOREIGN KEY (movie_id) REFERENCES movies(movie_id),
+	CONSTRAINT FK_movies_genre_genre FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+);
+
+COMMIT;
