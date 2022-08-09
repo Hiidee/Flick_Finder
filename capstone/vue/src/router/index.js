15c378debd4226044c6a84d336dc6822ae5dceb8
Croix <carichards1998@gmail.com>
2022-08-09 14:58:50 -0400

    some vue design

--- capstone/vue/src/router/index.js
+++ capstone/vue/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import FlickFinder from '@/views/FlickFinder.vue'
 
 Vue.use(Router)
 
@@ -51,6 +52,14 @@ const router = new Router({
       component: Register,
       meta: {
         requiresAuth: false
+      }
+    },
+    {
+      path: "/flickfinder",
+      name: 'flickfinder',
+      component: FlickFinder,
+      meta: {
+        requiresAuth: true
       }
     }
   ]
