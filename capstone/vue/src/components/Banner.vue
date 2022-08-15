09b1d440383f19da93cc85b291aa8ca748722c9b
Benjamin Hom <benh8642@gmail.com>
2022-08-15 15:47:52 -0400

    banner fixed, movie card updated

--- capstone/vue/src/components/Banner.vue
+++ capstone/vue/src/components/Banner.vue
@@ -1,58 +1,62 @@
 <template>
   <header>
     <div id="nav">
-      <div class = "banner-button">
-      <router-link class = "link" v-bind:to="{ name: 'home' }">Home</router-link>
-      </div>
-      <div class = "banner-button" v-if="$store.state.token != ''">
-      <router-link class = "link"
-        v-bind:to="{ name: 'logout' }"
-        >Logout</router-link>
-      </div>
-      <div class = "banner-button">
-      <router-link class = "link" v-bind:to="{ name: 'browse' }">Browse</router-link>
-      </div>
-      <div class = "banner-button">
-      <router-link class = "link" v-bind:to="{ name: 'flickFinder' }">Flick Finder</router-link>
-      </div>
-      <div class = "banner-button" v-if="$store.state.token != ''">
-      <router-link class = "link" v-bind:to="{ name: 'profile' }"
-      >Profile</router-link>
-      </div>
+      <router-link class="link" v-bind:to="{ name: 'home' }"
+        ><div class="banner-button">Home</div></router-link
+      >
+      <router-link class="link" v-bind:to="{ name: 'flickFinder' }"
+        ><div class="banner-button">Flick Finder</div></router-link
+      >
+      <router-link class="link" v-bind:to="{ name: 'browse' }"
+        ><div class="banner-button">Browse</div></router-link
+      >
+      <router-link class="link" v-bind:to="{ name: 'profile' }"
+        ><div class="banner-button" v-if="$store.state.token != ''">
+          Profile
+        </div></router-link
+      >
+      <router-link class="link" v-bind:to="{ name: 'logout' }"
+        ><div class="banner-button" v-if="$store.state.token != ''">
+          Logout
+        </div></router-link
+      >
     </div>
   </header>
 </template>
 
 <script>
 export default {
-    name: 'banner'
+  name: "banner",
 };
 </script>
 
 <style>
-#nav, .banner-button {
+#nav,
+.banner-button {
   padding-bottom: 10;
   display: flex;
   justify-content: space-around;
-  
 }
 
-.banner-button{
-  background-color: #418883;
+.banner-button {
   justify-content: center;
   flex-grow: 0.075;
   padding: 10px;
   border-radius: 5px;
-  color: whitesmoke;
+  color: white;
 }
 
-#nav{
-background-color: darkblue;
-padding: 20px;
+.banner-button:hover {
+  background-color: white;
+  color: black;
 }
 
-.link{
-  color:black;
+#nav {
+  background-color: darkblue;
+  padding: 20px;
 }
 
+.link {
+  color: black;
+}
 </style>
